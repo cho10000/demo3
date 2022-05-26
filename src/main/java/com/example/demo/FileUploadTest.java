64a841f65fab39755fad70c505934b0342c5fe69
choah <choah@192.168.32.1>
2022-05-26 11:25:45 +0900

    TEST

---
+++ src/main/java/com/example/demo/FileUploadTest.java
@@ -0,0 +1,20 @@
+ package com.example.demo;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class FileUploadTest extends WebIntegrationTest {
+	@DisplayName("1. 파일 한개 업로드")
+	@Test
+	void test_1() {
+			
+	}
+	
+	@DisplayName("2. 파일 두개 업로드")
+	@Test
+	void test_2() {
+			
+	}
+
+
+}
